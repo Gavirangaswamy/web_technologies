7bddc566582097b1367a77fbdba4d020803f6178
Gavirangaswamy <92967367+Gavirangaswamy@users.noreply.github.com>
2021-12-04 00:46:10 +0530

    Add files via upload

--- Login&Signup/js/index.js
+++ Login&Signup/js/index.js
@@ -2,21 +2,25 @@ function loginValidation(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var msg = document.getElementById("login");
+    
 
     if(username!==""){
         if(username.length<6 || username.length>12 ){
-           msg.innerHTML="Username is invalid";
+           msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Username is invalid";
         }else if(password===""){
-            msg.innerHTML="Password is Empty";
+            msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Password is Empty";
         }else if(password.length<5){
-            msg.innerHTML="Password is Week";
+            msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Password is Week";
         }else if(password.length>5 && password.length<8){
-            msg.innerHTML="Password is Strong";
+            msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Password is Strong";
         }else{
             msg.innerHTML="Login Success";
+            if(msg.innerHTML==="Login Success"){
+            msg.style.color="green";
+            }
         }
     }else{
-        msg.innerHTML="Username is Empty";
+        msg.innerHTML= '<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"  Username is Empty";
     }
 }
 function regValidation(){
@@ -31,28 +35,31 @@ function regValidation(){
 
     if(username!==""){
         if(username.length<6 || username.length>12 ){
-            msg.innerHTML="Username is invalid";
+            msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Username is invalid";
          }else if(fName===""){
-            msg.innerHTML="First Name is Empty";
+            msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   First Name is Empty";
          }else if(lName===""){
-            msg.innerHTML="Last Name is Empty";
+            msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Last Name is Empty";
          }else if(password===""){
-             msg.innerHTML="Password is Empty";
+             msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Password is Empty";
          }else if(password.length<5){
-             msg.innerHTML="Password is Week";
+             msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Password is Week";
          }else if(password.length>5 && password.length<8){
-             msg.innerHTML="Password is Strong";
+             msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Password is Strong";
          }else if(cnfpassword!==password){
-            msg.innerHTML = "password mismatch";
+            msg.innerHTML = '<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   password mismatch";
         }else if(phone.length<10){
-            msg.innerHTML = "Invalid Phone Number";
+            msg.innerHTML = '<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Invalid Phone Number";
         }else if(email.length<5 || email.length>20){
-            msg.innerHTML = "Invalid mailid";
+            msg.innerHTML = '<i class="fas fa-exclamation-circle" style ="font-size:20px;"></i>'+"   Invalid mailid";
         }else{
             msg.innerHTML = "Registration Successful"; 
+            if(msg.innerHTML==="Registration Successful"){
+                msg.style.color="green";
+                }
         }
     }else{
-        msg.innerHTML="Username is Empty";
+        msg.innerHTML='<i class="fas fa-exclamation-circle" style ="font-size:20px"></i>'+ " Username is Empty";
     }
     
 }
